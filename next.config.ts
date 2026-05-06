923f720efacaadd880b5c94a69adb45535dc194b
user <irshadparakkat@gmail.com>
2026-05-06 14:30:32 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
   },
 };
 
